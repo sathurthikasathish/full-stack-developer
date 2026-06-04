c2312d32784a6e2b04bb5db04849c852bc72fd1a
sathurthika <sathurthikasathish24@gmail.com>
2026-06-04 15:46:43 +0530

    js completed

---
+++ CSS/Javascript/script.js
@@ -0,0 +1,106 @@
+
+// var a=10;//Reinitialize & Redeclare
+// let b=20;//Reinitialize
+// const c=30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str = "sathu";
+// var undefine;
+// var Null = null;
+// var boolean = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof Null);
+// console.log(typeof undefine);
+// console.log(typeof bolean);
+
+// var height = 123n;
+// var symbol = Symbol('li');
+
+// console.log(typeof BigInt);
+// console.log(typeof symbol);
+
+// var arr=[10,20,30,40];
+// console.log(typeof arr, arr);
+// var obj = {
+//     name:"Vikram",
+//     dept:["AIDS","CT"]
+// }
+// console.log(typeof obj,obj);
+
+ 
+// //Arithmetic operator(+,-,*,%,/)
+// var a = 10;
+// var b = "hemasoni"
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+
+// //logical operator(&&,||,!);
+// //                      &&    ||    !
+// //true false          true  true  false
+// //true false          false  true true
+// //false false         false  false
+// //false true          false  true
+
+
+// // var a ="true";
+// // var b = "false";
+// // console.log(a&&b);
+// // console.log(!a);
+
+
+// //relational operator(<,>,<=,>=,==,!=)
+
+
+// a=10;
+// b=20;
+// //  console.log(a>b);
+// //  console.log(a<b);
+// //  console.log(a<=b);
+// //  console.log(a==b);
+// //  console.log(a>=b);
+// //  console.log(a!=b);
+// //  console.log(a===b);
+// //  console.log(a!==b);
+  
+
+// a=10;
+// b=20;
+ 
+// a+=b //a=a+b;
+
+// // console.log(a);
+// // a-=b; //a=a-b;
+// // console.log(a);
+// // a*=b; //a=a*b;
+// // console.log(a);
+
+// a%=b; //a=a%b;
+// console.log(a);
+// a/=b; //a=a/b;
+  
+// var a= 10;
+// console.log(a++);
+// console.log(++a);
+
+// var a=20;
+// console.log(--a);
+// console.log(a--);
+
+
+a = 1;
+b = 1;
+c = 0;
+
+
+var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+console.log(result)
+
