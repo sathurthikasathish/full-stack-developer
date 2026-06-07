1572ce8c946e297fda7602828b6710cff8553a13
sathurthika <sathurthikasathish24@gmail.com>
2026-06-07 10:08:35 +0530

    React project promps and state completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 
-import Counter from './components/counter'
-import Counters from './components/Counters'
+import Counter from './components/Counter'
 import Navbar from './components/Navbar'
 import Abouts from './components/Pages/Abouts'
 import Contact from './components/Pages/Contact'
 import Home from './components/Pages/Home'
 import Parent from './components/Pages/Parent'
 import Service from './components/Pages/Service'
-import parent from './components/parent'
+
 const App = () => {
   return (
     //Fragment Tag
@@ -20,7 +19,7 @@ const App = () => {
     <Abouts/>
     <Contact/>
     <Parent/>
-    <Counters/>
+    <Counter/>
     </>
   )
 }
